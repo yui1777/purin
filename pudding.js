764828e348dfc0ed89d2c5440a3a6a518db7a495
yui1777 <69725326+yui1777@users.noreply.github.com>
2020-08-24 21:52:07 +0900

    Update pudding.js

--- pudding.js
+++ pudding.js
@@ -5,6 +5,18 @@ const app = express();
 const env = process.env;
 require('dotenv').config({path: `ignore/.env`});
 
+const DB_HOST = env.DB_HOST;
+const DB_USERNAME = env.DB_USERNAME;
+const DB_PASSWORD=env.DB_PASSWORD;
+const DB_DATABASE = env.DB_DATABASE;
+
+const connection = mysql.createConnection({
+  host:DB_HOST,
+  user:DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+});
+
 
 
 connection.connect((err) => {
@@ -26,7 +38,7 @@ app.get('/', (req, res) => {
 app.post('/purin2', (req, res) => {
     // データベースに追加する処理を書いてください
     connection.query(
-      'insert into db_database(name,username) values(?,?)',
+      'insert into purin_type2(name,username) values(?,?)',
       [req.body.purinname,req.body.username],
       (error,results)=>{
           res.render('purin2.ejs');
